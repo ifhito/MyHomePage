88d42f41bf2a2bf3c08537bcb9108b383a5c6ee2
ayumu seki <a.s.0208@uec.ac.jp>
2020-04-17 01:21:34 +0900

    Updates

---
+++ precache-manifest.d217cd90f5a26c42519fc0fa62e1b2d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e76010e3d96663b689c90e44a68e8c",
+    "url": "/MyHomePage/index.html"
+  },
+  {
+    "revision": "4f4c6da5751b3d76ef4b",
+    "url": "/MyHomePage/static/css/main.28bfc454.chunk.css"
+  },
+  {
+    "revision": "027b35c43301014006d5",
+    "url": "/MyHomePage/static/js/2.b041f846.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyHomePage/static/js/2.b041f846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f4c6da5751b3d76ef4b",
+    "url": "/MyHomePage/static/js/main.bc41e576.chunk.js"
+  },
+  {
+    "revision": "7ffee9ce3d8ffba286dc",
+    "url": "/MyHomePage/static/js/runtime-main.0dd4fece.js"
+  },
+  {
+    "revision": "eefc0fd25f76a11882e20b547c77291c",
+    "url": "/MyHomePage/static/media/futura.eefc0fd2.ttf"
+  }
+]);
